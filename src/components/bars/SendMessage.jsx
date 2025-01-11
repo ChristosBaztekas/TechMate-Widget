c36bdacb96a143e5eccc59b11b122922dfb1baaf
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2025-01-11 16:36:42 +0200

    Enhance Send Massage bar Logic

--- src/components/bars/SendMessage.jsx
+++ src/components/bars/SendMessage.jsx
@@ -1,15 +1,20 @@
 export const SendMessage = () => {
     return (
-        <main className="flex h-[75px] px-6 justify-center items-center bg-gradient-to-r from-primaryColor to-gradientColor h-18">
-            {/* Input field for typing the message */}
-            <input
-                type="text"
+        <main className="flex p-6 justify-center items-start bg-gradient-to-r from-primaryColor to-gradientColor h-18">
+            {/* Textarea for typing the message */}
+            <textarea
                 placeholder="Πληκτρολογήστε την ερώτησή σας..."
-                className="w-full h-10 rounded-[20px] px-7 outline-none"
+                className="w-full min-h-10 rounded-[20px] pl-5 p-2 outline-none resize-none overflow-hidden"
                 aria-label="Message input field"
+                rows={1} // Initial height of the textarea
+                onInput={(e) => {
+                    // Adjust the height of the textarea based on its content
+                    e.target.style.height = 'auto';
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                }}
             />
             {/* Submit button for sending the message */}
-            <button type="submit" className="ml-6 text-white font-bold">
+            <button type="submit" className="ml-6 mt-2 text-white font-bold">
                 Send
             </button>
         </main>
