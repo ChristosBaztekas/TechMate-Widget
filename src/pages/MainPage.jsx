96604f775e855bc1e4edbc38d81793aab97a191f
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2025-02-21 01:30:44 +0200

    Add Widget Feature

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,34 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllQuestions } from "@/store/Slices/chatbotApiSlice";
 import { setChatState, setNotificationState } from "@/store/Slices/userSlice";
 import { sendDimensionsToParent } from "@/utils/functions.util"; // Import sendDimensionsToParent function
-import { Notifications } from "@/components/Notifications"; // Import Notifications component
 import { StartPage } from "@/pages"; // Import StartPage component
 import Logo from "@/assets/images/Logo.webp"; // Import logo image
+// Components
+import { Widget } from "../components/Widget";
+import { Notifications } from "@/components/Notifications";
 
 export const MainPage = () => {
   const dispatch = useDispatch();
   const { isChatClosed, notification } = useSelector((state) => state.user);
   const NOTIFICATION_DELAY = 2000; // Delay of 2 seconds
+  const [isWidgetClosed, setIsWidgetClosed] = useState(false); // State to manage widget visibility
 
   // Effect to update parent with chat dimensions based on its state
   useEffect(() => {
-    if (isChatClosed && notification) {
-      sendDimensionsToParent("290px", "220px", true);
+    if (!isWidgetClosed) {
+      sendDimensionsToParent("200px", "230px", false, isWidgetClosed);
+    } else if (isChatClosed && notification) {
+      sendDimensionsToParent("290px", "220px", true, isWidgetClosed);
     } else if (isChatClosed) {
-      sendDimensionsToParent("95px", "87px", true);
+      sendDimensionsToParent("95px", "87px", true, isWidgetClosed);
     } else {
-      sendDimensionsToParent("33%", "588px", false);
+      sendDimensionsToParent("33%", "588px", false, isWidgetClosed);
     }
-  }, [isChatClosed, notification]);
+  }, [isChatClosed, notification, isWidgetClosed]);
 
   // Effect to show notification after a delay
   useEffect(() => {
     const timer = setTimeout(
       () => dispatch(setNotificationState(true)),
       NOTIFICATION_DELAY
-    ); // Delay of 1 second
+    ); // Delay of 2 seconds
     return () => clearTimeout(timer); // Cleanup the timer if the component is unmount
   }, [dispatch]);
 
@@ -36,28 +41,36 @@ export const MainPage = () => {
     dispatch(fetchAllQuestions());
   }, [dispatch]);
 
+  // Handle widget close action
+  const handleWidgetClose = () => {
+    setIsWidgetClosed(true);
+  };
+
+  // Render Widget first, then the rest of the content based on isWidgetClosed state
   return (
     <>
-      {!isChatClosed ? (
-        <StartPage />
+      {!isWidgetClosed ? (
+        <Widget onClose={handleWidgetClose} />
       ) : (
         <>
-          {notification && <Notifications />}
-          <button
-            onClick={() => {
-              dispatch(setChatState(false));
-            }}
-            className="fixed m-3 bottom-0 right-0 z-50 flex justify-center text-sm items-center w-16 h-16 bg-gray-400 rounded-full cursor-pointer"
-            role="button"
-            aria-label="Open chat"
-          >
-            {/* Logo */}
-            <img
-              src={Logo}
-              alt="logo"
-              className="absolute -top-1 -left-7 w-16" loading="lazy"
-            />
-          </button>
+          {!isChatClosed ? (
+            <StartPage />
+          ) : (
+            <>
+              {notification && <Notifications />}
+              <button
+                onClick={() => {
+                  dispatch(setChatState(false));
+                }}
+                className="fixed m-3 bottom-0 right-0 z-50 flex justify-center text-sm items-center w-16 h-16 bg-gray-400 rounded-full cursor-pointer"
+                role="button"
+                aria-label="Open chat"
+              >
+                {/* Logo */}
+                <img src={Logo} alt="logo" className="absolute -top-1 -left-7 w-16" loading="lazy" />
+              </button>
+            </>
+          )}
         </>
       )}
     </>
