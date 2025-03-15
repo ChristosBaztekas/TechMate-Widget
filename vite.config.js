28c0bf28fc03b9cdce1792f0a733030df2a7ddd2
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2025-03-15 13:32:12 +0200

    ADD env file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
+  // Load environment variables
   const env = loadEnv(mode, process.cwd());
 
   return {
@@ -14,9 +15,8 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: "https://api-test.techmate.gr/", // Use env variable 
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
-          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
